70c011992eab74286ab558eb156f079d876696ea
Adam Campbell <adamrobcampbell@gmail.com>
2018-12-17 10:58:23 +1300

    Uploading base files

---
+++ interpolation.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Lagrange polynomial prototyping for bicubic interpolation of two dimensional matrices
+
+int main(int argc, char** argv) {
+
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ interpolation.h
@@ -0,0 +1,17 @@
+
+
+#ifndef INTERPOLATION_H
+#define INTERPOLATION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERPOLATION_H */
+
